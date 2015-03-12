c244c19a2450763d316b378004e87fc0e37f8609
santiaago <santiago.arias@red-gate.com>
2015-03-12 16:08:40 +0000

    passengers print func

--- titanic/passenger.go
+++ titanic/passenger.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type passenger struct {
 	ID       string
 	Survived bool
@@ -13,4 +15,20 @@ type passenger struct {
 	Fare     string
 	Cabin    string
 	Embarked string
+}
+
+func (p passenger) Print() {
+	fmt.Println("ID", p.ID)
+	fmt.Println("Survived", p.Survived)
+	fmt.Println("Pclass", p.Pclass)
+	fmt.Println("Name", p.Name)
+	fmt.Println("Sex", p.Sex)
+	fmt.Println("Age", p.Age)
+	fmt.Println("SibSp", p.SibSp)
+	fmt.Println("Sex", p.Parch)
+	fmt.Println("Ticket", p.Ticket)
+	fmt.Println("Cabin", p.Cabin)
+	fmt.Println("Fare", p.Fare)
+	fmt.Println("Cabin", p.Cabin)
+	fmt.Println("Embarked", p.Embarked)
 }
