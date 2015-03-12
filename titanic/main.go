2c04e486f63aaf461603eb72178812a57e397acc
santiaago <santiago.arias@red-gate.com>
2015-03-12 15:18:09 +0000

    Read passengers data

--- titanic/main.go
+++ titanic/main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 )
 
 var (
@@ -31,9 +32,41 @@ func trainModel(r *csv.Reader) {
 		log.Fatalln(err)
 	} else {
 		//passengers := []passenger{}
-		for i := 1; i < 3; /*len(rawData)*/ i++ {
-			fmt.Println(rawData[i][0], rawData[i][3])
-			//p := passenger{rawData[i]}
+		for i := 1; i < len(rawData); i++ {
+			p := passengerFromLine(rawData[i])
+			fmt.Printf("%+v\n", p)
 		}
 	}
+}
+
+func passengerFromLine(line []string) passenger {
+	survived, err := strconv.ParseBool(line[1])
+	if err != nil {
+		survived = false
+	}
+	age, err := strconv.ParseInt(line[5], 10, 32)
+	if err != nil {
+		age = 33
+	}
+
+	var embarked string
+	if len(line) > 11 {
+		embarked = line[11]
+	}
+
+	p := passenger{
+		line[0],
+		survived,
+		line[2],
+		line[3],
+		line[4],
+		int(age),
+		line[6],
+		line[7],
+		line[8],
+		line[9],
+		line[10],
+		embarked,
+	}
+	return p
 }
