fe2be1e141f9999a6394a028d04b1a7424416dae
santiaago <santiago.arias@red-gate.com>
2015-03-12 17:07:53 +0000

    Read test data and fill predictions

--- titanic/main.go
+++ titanic/main.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"strconv"
 
+	"github.com/santiaago/caltechx.go/linear"
 	"github.com/santiaago/caltechx.go/linreg"
 )
 
@@ -19,28 +20,79 @@ var (
 func main() {
 	flag.Parse()
 
+	var linreg *linreg.LinearRegression
 	// train
 	if csvfile, err := os.Open(*train); err != nil {
 		log.Fatalln(err)
 	} else {
 		reader := csv.NewReader(csvfile)
-		trainModel(reader)
+		linreg = trainModel(reader)
 	}
 	// test ...
-}
-
-func trainModel(r *csv.Reader) {
-	if rawData, err := r.ReadAll(); err != nil {
+	if csvfile, err := os.Open(*test); err != nil {
 		log.Fatalln(err)
 	} else {
-		passengers := []passenger{}
-		for i := 1; i < len(rawData); i++ {
-			p := passengerFromTrainLine(rawData[i])
-			passengers = append(passengers, p)
+		reader := csv.NewReader(csvfile)
+		testModel(reader, linreg)
+	}
+}
+
+func trainModel(r *csv.Reader) *linreg.LinearRegression {
+	var rawData [][]string
+	var err error
+	if rawData, err = r.ReadAll(); err != nil {
+		log.Fatalln(err)
+		return nil
+	}
+	passengers := []passenger{}
+	for i := 1; i < len(rawData); i++ {
+		p := passengerFromTrainLine(rawData[i])
+		passengers = append(passengers, p)
+	}
+	return linregSexAge(passengers)
+	//linregPClassAge(passengers)
+	//linregPClassSex(passengers)
+
+}
+
+func testModel(r *csv.Reader, linreg *linreg.LinearRegression) {
+	var rawData [][]string
+	var err error
+	if rawData, err = r.ReadAll(); err != nil {
+		log.Fatalln(err)
+	}
+	passengers := []passenger{}
+	for i := 1; i < len(rawData); i++ {
+		p := passengerFromTestLine(rawData[i])
+		passengers = append(passengers, p)
+	}
+	linregTestSexAge(linreg, &passengers)
+}
+
+func linregTestSexAge(linreg *linreg.LinearRegression, passengers *[]passenger) {
+	var data [][]float64
+	for i := 0; i < len(*passengers); i++ {
+		p := (*passengers)[i]
+
+		var sex float64
+		if p.Sex == "female" {
+			sex = float64(1)
 		}
-		linregSexAge(passengers)
-		linregPClassAge(passengers)
-		linregPClassSex(passengers)
+		d := []float64{sex, float64(p.Age), float64(0)}
+		data = append(data, d)
+	}
+
+	for i := 0; i < len(data); i++ {
+		oX := data[i][:1]
+		gi := float64(0)
+		for j := 0; j < len(oX); j++ {
+			gi += oX[j] * linreg.Wn[j]
+		}
+
+		if linear.Sign(gi) > 0 {
+			data[i][2] = 1
+			(*passengers)[i].Survived = true
+		}
 	}
 }
 
@@ -154,6 +206,42 @@ func passengerFromTrainLine(line []string) passenger {
 		line[8],
 		line[9],
 		line[10],
+		embarked,
+	}
+	return p
+}
+
+func passengerFromTestLine(line []string) passenger {
+	id := line[0]
+	pclass := line[1]
+	name := line[2]
+	sex := line[3]
+	age, err := strconv.ParseInt(line[4], 10, 32)
+	if err != nil {
+		age = 33
+	}
+	sibsp := line[5]
+	parch := line[6]
+	ticket := line[7]
+	fare := line[8]
+	cabin := line[9]
+	var embarked string
+	if len(line) > 10 {
+		embarked = line[10]
+	}
+
+	p := passenger{
+		id,
+		false,
+		pclass,
+		name,
+		sex,
+		int(age),
+		sibsp,
+		parch,
+		ticket,
+		fare,
+		cabin,
 		embarked,
 	}
 	return p
