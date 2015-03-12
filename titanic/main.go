f48a945129c0a292a578f421dee7634fcb57d75a
santiaago <santiago.arias@red-gate.com>
2015-03-12 14:17:37 +0000

    main reading data mechanism

--- titanic/main.go
+++ titanic/main.go
@@ -1,8 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strings"
+)
+
+var (
+	test  = flag.String("test", "data/test.csv", "testing set")
+	train = flag.String("train", "data/train.csv", "training set")
+)
 
 func main() {
-	fmt.Println("hello titanic")
+	flag.Parse()
 
+	// train
+	if data, err := ioutil.ReadFile(*train); err != nil {
+		log.Fatalln(err)
+	} else {
+		reader := strings.NewReader(fmt.Sprintf("%s", data))
+		trainModel(reader)
+	}
+	// test ...
+}
+
+func trainModel(r *strings.Reader) {
+	fmt.Println("train")
 }
