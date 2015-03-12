64c9a05e54ebd9f2795f9c90d2648edfdc442c44
santiaago <santiago.arias@red-gate.com>
2015-03-12 13:50:57 +0000

    Add passenger structure

---
+++ titanic/passenger.go
@@ -0,0 +1,16 @@
+package main
+
+type passenger struct {
+	ID       string
+	Survived bool
+	Pclass   string
+	Name     string
+	Sex      string
+	Age      int
+	SibSp    string
+	Parch    string
+	Ticket   string
+	Fare     string
+	Cabin    string
+	Embarked string
+}
