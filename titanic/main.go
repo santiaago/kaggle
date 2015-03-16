b09b187a1b9ebe575cad1cbb96f22eabd676241a
santiaago <santiago.arias@red-gate.com>
2015-03-16 16:38:11 +0000

    apply it to data

--- titanic/main.go
+++ titanic/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/csv"
 	"flag"
+	"fmt"
 	"log"
 	"os"
 
@@ -24,21 +25,33 @@ func main() {
 	if csvfile, err := os.Open(*train); err != nil {
 		log.Fatalln(err)
 	} else {
-		reader := csv.NewReader(csvfile)
-
 		linregFuncs := []func(passengers []passenger) *linreg.LinearRegression{
 			linregSexAge,
 			linregPClassAge,
 			linregPClassSex,
+			linregSexAgePClass,
 		}
-
 		linregsNames = []string{
 			"data/testModel-SexAge.csv",
 			"data/testModel-PClassAge.csv",
 			"data/testModel-PClassSex.csv",
+			"data/testModel-SexAgePClass.csv",
 		}
+		linregs = trainModels(csv.NewReader(csvfile), linregFuncs)
+	}
 
-		linregs = trainModels(reader, linregFuncs)
+	if csvfile, err := os.Open(*train); err != nil {
+		log.Fatalln(err)
+	} else {
+		metaLinregFuncs := []func(passengers []passenger) []*linreg.LinearRegression{
+			linregVectorsOf3,
+		}
+		linregsV3 := trainModelsMeta(csv.NewReader(csvfile), metaLinregFuncs)
+		for i := 0; i < len(linregsV3); i++ {
+			name := fmt.Sprintf("data/testModel-V3-%d", i)
+			linregsNames = append(linregsNames, name)
+		}
+		linregs = append(linregs, linregsV3...)
 	}
 
 	// test models ...
@@ -51,6 +64,7 @@ func testModels(file string, linregs []*linreg.LinearRegression, linregsNames []
 			log.Fatalln(err)
 		} else {
 			reader := csv.NewReader(csvfile)
+			fmt.Println(linregs[i].Wn)
 			testModel(reader, linregs[i], linregsNames[i])
 
 		}
