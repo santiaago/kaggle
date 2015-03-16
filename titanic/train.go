2e3c61a0a5b2efdf584fc461736e1aa4d8fec2de
santiaago <santiago.arias@red-gate.com>
2015-03-16 16:37:55 +0000

    Add the trainModel for funcs like linregVectorsOf3

--- titanic/train.go
+++ titanic/train.go
@@ -24,5 +24,23 @@ func trainModels(r *csv.Reader, linregFuncs []func(passengers []passenger) *linr
 		linregs = append(linregs, f(passengers))
 	}
 	return linregs
+}
 
+func trainModelsMeta(r *csv.Reader, metaLinregFuncs []func(passengers []passenger) []*linreg.LinearRegression) []*linreg.LinearRegression {
+	var rawData [][]string
+	var err error
+	if rawData, err = r.ReadAll(); err != nil {
+		log.Fatalln(err)
+		return nil
+	}
+	passengers := []passenger{}
+	for i := 1; i < len(rawData); i++ {
+		p := passengerFromTrainLine(rawData[i])
+		passengers = append(passengers, p)
+	}
+	var linregs []*linreg.LinearRegression
+	for _, f := range metaLinregFuncs {
+		linregs = append(linregs, f(passengers)...)
+	}
+	return linregs
 }
