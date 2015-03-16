b08cec3d16fb8de5e983d20d5364e7c9c788bf13
santiaago <santiago.arias@red-gate.com>
2015-03-16 16:36:53 +0000

    linregVectorsOf3 creates an array of linreg models
    computes an array of all the combinations of a vector
    creates an array of all the linear reg models from each selected vector

--- titanic/linreg.go
+++ titanic/linreg.go
@@ -9,7 +9,8 @@ import (
 )
 
 //
-// PassengerId,Survived,Pclass,Name,Sex,Age,SibSp,Parch,Ticket,Fare,Cabin,Embarked
+// PassengerId,Survived,
+// Pclass,Name,Sex,Age,SibSp,Parch,Ticket,Fare,Cabin,Embarked
 //
 
 func linregTest(linreg *linreg.LinearRegression, passengers *[]passenger) {
@@ -37,6 +38,114 @@ func linregTest(linreg *linreg.LinearRegression, passengers *[]passenger) {
 			(*passengers)[i].Survived = true
 		}
 	}
+}
+
+// creates a linear regression model for each combination of
+// vector of 3 features and returns an array of linear regressions.
+func linregVectorsOf3(passengers []passenger) []*linreg.LinearRegression {
+	var data [][]float64
+	for i := 0; i < len(passengers); i++ {
+		p := passengers[i]
+
+		var survived float64
+		if p.Survived {
+			survived = float64(1)
+		}
+
+		var pclass float64
+		if pc, err := strconv.ParseInt(p.Pclass, 10, 32); err != nil {
+			pclass = float64(3)
+		} else {
+			pclass = float64(pc)
+		}
+
+		var sex float64
+		if p.Sex == "female" {
+			sex = float64(1)
+		}
+
+		var age = float64(p.Age)
+
+		var sibsp = float64(p.SibSp)
+		var parch = float64(p.Parch)
+		//var ticket = float64(p.Ticket)
+		var fare = float64(p.Fare)
+		//var cabin = float64(p.Cabin)
+
+		var embarked float64
+		if len(p.Embarked) == 0 {
+			embarked = float64(0)
+		} else if p.Embarked == "C" {
+			embarked = float64(0)
+		} else if p.Embarked == "Q" {
+			embarked = float64(1)
+		} else if p.Embarked == "S" {
+			embarked = float64(2)
+		}
+
+		d := []float64{sex, pclass, age, sibsp, parch, fare, embarked, survived}
+		data = append(data, d)
+	}
+	var linregs []*linreg.LinearRegression
+	combs := combinations([]int{0, 1, 2, 3, 4, 5, 6}, 3)
+	for _, comb := range combs {
+		fmt.Println(data[0])
+		filteredData := filter(data, comb)
+		fmt.Println(filteredData[0])
+		linreg := linreg.NewLinearRegression()
+		linreg.InitializeFromData(filteredData)
+		linreg.Learn()
+		fmt.Println("using combination", comb)
+		fmt.Printf("number of passengers: %d\n", len(passengers))
+		fmt.Printf("EIn = %f\n", linreg.Ein())
+		linregs = append(linregs, linreg)
+	}
+	return linregs
+}
+
+func filter(data [][]float64, combination []int) [][]float64 {
+	var filtered [][]float64
+	for i := 0; i < len(data); i++ {
+		var df []float64
+		for j := 0; j < len(combination); j++ {
+			df = append(df, data[i][combination[j]])
+		}
+		df = append(df, data[i][len(data[i])-1])
+		filtered = append(filtered, df)
+	}
+	return filtered
+}
+
+func linregSexAgePClass(passengers []passenger) *linreg.LinearRegression {
+	var data [][]float64
+	for i := 0; i < len(passengers); i++ {
+		p := passengers[i]
+		var survived float64
+		if p.Survived {
+			survived = float64(1)
+		}
+
+		var sex float64
+		if p.Sex == "female" {
+			sex = float64(1)
+		}
+
+		var pclass float64
+		if pc, err := strconv.ParseInt(p.Pclass, 10, 32); err != nil {
+			pclass = float64(3)
+		} else {
+			pclass = float64(pc)
+		}
+
+		d := []float64{sex, float64(p.Age), pclass, survived}
+		data = append(data, d)
+	}
+	linreg := linreg.NewLinearRegression()
+	linreg.InitializeFromData(data)
+	linreg.Learn()
+	fmt.Printf("number of passengers: %d\n", len(passengers))
+	fmt.Printf("EIn = %f\n", linreg.Ein())
+	return linreg
 }
 
 func linregSexAge(passengers []passenger) *linreg.LinearRegression {
