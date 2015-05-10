097411ac16e637ff11ada9ef2f8f6ec44449cf21
santiaago <santiago.ariassar@gmail.com>
2015-05-10 16:18:37 +0100

    clean test.go

--- titanic/test.go
+++ titanic/test.go
@@ -9,37 +9,35 @@ import (
 	"github.com/santiaago/ml/logreg"
 )
 
-// testModel runs a linear regression model on the data passed in the reader.
+// testModels runs a linear regression model on the data passed in the reader.
 // Then makes the predictions and write the predicted data to file using the
 // linear regression model name.
 // testModels run a test file for each linear regression model passed in the linreg array.
+//
 func testModels(r data.Reader, w data.Writer, models ml.ModelContainers) {
+
 	var dc data.Container
 	var err error
-	dc, err = r.Read()
-	if err != nil {
+
+	if dc, err = r.Read(); err != nil {
 		log.Println("error when getting data from reader,", err)
 	}
 
 	for _, m := range models {
+
 		if m == nil {
 			continue
 		}
-		_, ok := m.Model.(*linreg.LinearRegression)
-		if ok {
-			predictions, err := linregTest(m, dc)
-			if err == nil {
+
+		switch m.Model.(type) {
+		case *linreg.LinearRegression:
+			if predictions, err := linregTest(m, dc); err == nil {
 				w.Write(m.Name, predictions)
 			}
-			continue
-		}
-		_, ok = m.Model.(*logreg.LogisticRegression)
-		if ok {
-			predictions, err := logregTest(m, dc)
-			if err == nil {
+		case *logreg.LogisticRegression:
+			if predictions, err := logregTest(m, dc); err == nil {
 				w.Write(m.Name, predictions)
 			}
-			continue
 		}
 	}
 }
