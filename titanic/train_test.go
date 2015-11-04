31470cbe911d0e53ce9d560605e20d8ec281e5d4
santiaago <santiago.arias@red-gate.com>
2015-11-04 11:27:07 +0000

    test manual SVM creation

--- titanic/train_test.go
+++ titanic/train_test.go
@@ -9,8 +9,24 @@ import (
 )
 
 func buildContainer() (data.Container, error) {
-	reader := NewPassengerReader("data/test.csv", NewPassengerTrainExtractor())
+	reader := NewPassengerReader("data/train.csv", NewPassengerTrainExtractor())
 	return reader.Read()
+}
+
+func createSVM(t *testing.T, dc data.Container) *svm.SVM {
+	svm := svm.NewSVM()
+	svm.K = 20
+	svm.Lambda = 0.001
+	svm.T = 1000
+
+	features := []int{2, 4, 6, 7, 8, 9, 10}
+	fd := dc.FilterWithPredict(features)
+	svm.InitializeFromData(fd)
+
+	if err := svm.Learn(); err != nil {
+		t.Error(err)
+	}
+	return svm
 }
 func TestSVMImport(t *testing.T) {
 
@@ -20,15 +36,10 @@ func TestSVMImport(t *testing.T) {
 		t.Error(err)
 	}
 
-	features := []int{2, 4, 6, 7, 8, 9, 10}
-	fd := dc.FilterWithPredict(features)
-	svm := svm.NewSVM()
-	svm.K = 20
-	svm.Lambda = 0.001
-	svm.T = 1000
-	svm.InitializeFromData(fd)
-	if err := svm.Learn(); err != nil {
-		t.Error(err)
-	}
-	fmt.Println(svm.Wn)
+	svm := createSVM(t, dc)
+
+	strWn := fmt.Sprintf("From Code Wn: %v", svm.Wn)
+	fmt.Println(strWn)
+	strEin := fmt.Sprintf("From Code Ein: %v", svm.Ein())
+	fmt.Println(strEin)
 }
